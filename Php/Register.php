a451ed9cf516a3de112e4daa595a3747a29b6540
Wullmer <William@trollbacken.com>
2017-11-07 20:12:20 +0100

    Register.php
    Register lägger nu till en användare i databasen.

--- Php/Register.php
+++ Php/Register.php
@@ -75,22 +75,46 @@ li.dropdown {
 
 
 </ul>
+<?php 
+
+	session_start();
+	$_SESSION['message']='';
+	try{
+		$db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=adasaw5db', 'adasaw-5', '1234');
+	}
+	catch(Exception $e){
+		die;
+		echo $e->getMessage();
+	}
+	if($_SERVER['REQUEST_METHOD']=='POST'){
+
+		$stmt = $db->prepare("INSERT INTO Users (Username, Password, Namn, Adress, Roll) VALUES ('" . $_POST["username"] . "', '" . $_POST["password"] . "', '" . $_POST["Namn"] . "', '" . ($_POST["Address"]) . "', '" . 'pleb' . "')");
+		$stmt->execute();
+		
+		echo "<script> alert('You are now registered, thank you!'); window.location='/~adasaw-5/root/Index.html'; </script>";
+		
+
+		
+		
+	}
+
+	?>
+
 <body>
-    <form action="/action_page.php">
+    <form name="Registration" method="post" action="">
+		<div class="alert"> <?=$_SESSION['message']?></div>
         <div class="container">
             <label>Username</label>
-            <input type="text" placeholder="Enter Username" name="username" required>
-            <label>
-            Password</label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
-
-            <label>
-            Repeat Password</label>
-            <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-            <input type="checkbox" checked="checked"> Remember me
-        
-
-            <div class="clearfix">
+            <input type="text" placeholder="Enter Username" name="username" value="<?php if(isset($_POST['username'])) echo $_POST['username'];?>" required>
+			<label>Name</label>
+			<input type="text" placeholder="Enter Name" name="Namn" value="<?php if(isset($_POST['Namn'])) echo $_POST['Namn'];?>" required>
+			<label>Address</label>
+			<input type="text" placeholder="Enter Address" name="Address" value="<?php if(isset($_POST['Address'])) echo $_POST['Address'];?>" required>
+			<label>Password</label>
+            <input type="password" placeholder="Enter Password" name="password" value="<?php if(isset($_POST['password'])) echo $_POST['password'];?>" required>
+           
+			
+            <div class="clear">
                 <button type="button" class="cancelbtn">Cancel</button>
                 <button type="submit" class="signupbtn">Register</button>
             </div>
