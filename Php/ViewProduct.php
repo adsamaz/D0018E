8069cad54285def79cb0c6a7fbd71d4e29592c4a
adsamaz <adsamaz@github.com>
2017-11-11 01:26:43 +0100

    DisableButton funkar
    Kollar också om man är inloggad

--- Php/ViewProduct.php
+++ Php/ViewProduct.php
@@ -1,9 +1,11 @@
+<!DOCTYPE html>
+
 <?php session_start();?>
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="../Css/StandardStyle.css">
-	
-	
+    <script type="text/javascript" src="../Javascript/disableButtonOutOfStock.js"></script>
+
 </head>
 
 
@@ -11,12 +13,12 @@
 
 <?php
 	include "../Html/menu.html";
-	
+
     try{
 		$db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=adasaw5db', 'adasaw-5', '1234');
 	}
 	catch(Exception $e){
-		
+
 		echo $e->getMessage();
 	}
 
@@ -29,18 +31,13 @@
 
 	$lagerAntal = $row['LagerAntal'];
 
-	
-
-	//echo "</ul> <ul class='storeList'>";
-    //$id = $row['ProduktID'];
-    //echo "<a href='ViewProduct.php?ID=$id' class='LinkItem'>";
 
 	echo "<h3>" . $row['Namn'] . "</h3>";
 	echo "<br /> <b>Description</b> <br />" . $row['Beskrivning'];
 	echo "<br /><br /> <b>Price:</b> $" . $row['Pris'];
 	echo " <b>In Stock:</b> " . $row['LagerAntal'];
-	
-	
+
+
 	if($_SERVER['REQUEST_METHOD']=='POST'){
 		$random = rand();
 		$stmt = $db->prepare("INSERT INTO Ordrar (ID,Datum,Status,Username,Ok) VALUES ('" . ($random) . "', '" . date("Y-m-d") . "', '" . "ok" . "', '" . ($_SESSION['username']) . "','" . 0 . "') ON DUPLICATE KEY UPDATE ID=". rand());
@@ -49,35 +46,35 @@
 		$stmt1->execute();
 		$rowOP = $stmt1->fetch(PDO::FETCH_ASSOC);
 		$rowO = $stmt->fetch(PDO::FETCH_ASSOC);
-		
-		$_SESSION['ProductID'] = $id;	
-		
+
+		$_SESSION['ProductID'] = $id;
+
 		echo "<br> Added to your cart <br>";
 	}
 
+  ?>
+  <div id="largeOrder"> </div>
 
-	
-    ?>
-	<script type="text/javascript">
-	
-		var lagerAntal = "<?php echo $lagerAntal ?>";
-		
-		var oText = document.getElementById("antal");
-		console.log(doucment);
-		console.log(document.getElementById("antal"));
-		if (oText.value > lagerAntal) {
-			document.getElementById("btnSubmit").disabled = true;
-		}
-		else {
-			document.getElementById("btnSubmit").disabled = false;
-		}
-    
-	</script>
-	
+
+
 	<form action="" method="post">
-		<input type="text" id="antal" name="antal" value="<?php if(isset($_POST['antal'])) echo $_POST['antal'];?>">
-		<input type="submit" id="btnSubmit" name="btnSubmit" value="Add to cart">
+      <label for="antal">Amount:</label>
+  		<input type="text" onkeyup="checkAmount()" id="antal" name="antal" value="<?php if(isset($_POST['antal'])) echo $_POST['antal'];?>">
+  		<input type="submit" id="btnSubmit" name="btnSubmit" value="Add to cart" disabled>
 	</form>
+
+
+<?php
+//checks if username is set and creats the javascript variable lagerAntal
+  echo '<script type="text/javascript">';
+  echo 'var lagerAntal = ' . json_encode($lagerAntal) . ';';
+  if(!isset($_SESSION['username'])){
+    echo "document.getElementById('btnSubmit').disabled = true;";
+    echo "document.getElementById('antal').disabled = true;";
+    echo "document.getElementById('largeOrder').innerHTML = 'Log in to buy';";
+  }
+  echo '</script>';
+?>
 
 </body>
 </html>
