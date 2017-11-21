8cf61140bb3f35bfb2ffb7baa062d413973e95af
adsamaz <adsamaz@github.com>
2017-11-21 12:03:49 +0100

    smoke and css

--- Php/Store.php
+++ Php/Store.php
@@ -3,71 +3,73 @@
 <head>
     <link rel="stylesheet" type="text/css" href="../Css/StandardStyle.css">
     <link rel="stylesheet" type="text/css" href="../Css/StoreStyle.css">
-</head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript" src="../Javascript/Smoke.js"></script>
 
+</head>
 
 <body>
 
-<?php
-  if(isset($_SESSION['username'])){
-    include "../Html/LogIN.html";
-  }
-  else{
-    include "../Html/menu.html";
-  }
-?>
-  <div class="parallax"></div>
+  <?php
+    if(isset($_SESSION['username'])){
+      include "../Html/LogIN.html";
+    }
+    else{
+      include "../Html/menu.html";
+    }
+  ?>
+  <canvas class="parallax"></canvas>
   <div id="wrap">
-	<form action ="Store.php" method="post">
-			<input type="text" class="searchField" name="search" placeholder="Search" />
-			<input type="submit" class="searchButton" name="btnSearch" value="Search" />
-	</form>
-<?php
+    <div class="content">
+      <h1>Vapes</h1>
+    	<form action ="Store.php" method="post">
+    			<input type="text" class="searchField" name="search" placeholder="Search" />
+    			<input type="submit" class="searchButton" name="btnSearch" value="Search" />
+    	</form>
 
-    try{
-		$db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=adasaw5db', 'adasaw-5', '1234');
-	}
-	catch(Exception $e){
+      <?php
+      try{
+    		$db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=adasaw5db', 'adasaw-5', '1234');
+    	}
+    	catch(Exception $e){
 
-		echo $e->getMessage();
-	}
-	if(isset($_POST['search'])){
-		$searchValue = $_POST['search'];
-		$stmt = $db->prepare("SELECT * FROM Produkter WHERE kategori LIKE '%$searchValue%' OR Namn LIKE '%$searchValue%'");
-	}
-	else{
-	$stmt = $db->prepare("SELECT * FROM Produkter WHERE kategori = 'vape'");
-	}
-	$stmt->execute();
+    		echo $e->getMessage();
+    	}
+    	if(isset($_POST['search'])){
+    		$searchValue = $_POST['search'];
+    		$stmt = $db->prepare("SELECT * FROM Produkter WHERE kategori LIKE '%$searchValue%' OR Namn LIKE '%$searchValue%'");
+    	}
+    	else{
+    	   $stmt = $db->prepare("SELECT * FROM Produkter WHERE kategori = 'vape'");
+    	}
+    	$stmt->execute();
 
-	echo "<ul class='storeList'>";
-	$i = 1;
-	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-		if($i > 5){
-			$i = 1;
-			echo "</ul> <ul class='storeList'>";
-		}
-    $id = $row['ID'];
-    //echo "<a href='ViewProduct.php?ID=$id' class='LinkItem'>";
-    $ImageID = $row['Bild'];
+    	echo "<ul class='storeList'>";
+    	$i = 1;
+    	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    		if($i > 5){
+    			$i = 1;
+    			echo "</ul> <ul class='storeList'>";
+    		}
+        $id = $row['ID'];
+        $ImageID = $row['Bild'];
 
-			echo "<li><a href='ViewProduct.php?ID=$id' class='LinkItem'><h3>" . $row['Namn'] . "</h3>";
-      echo "<img src='../Images/ProductImage$ImageID.jpg' />";
-      //echo "<br /> <b>Description</b> <br />" . $row['Beskrivning'];
-			echo "<br /><br /> <b>Price:</b> $" . $row['Pris'];
-			echo " <b>In Stock:</b> " . $row['LagerAntal'] . "</a></li>";
-
-            //echo "</a>";
+  			echo "<li><a href='ViewProduct.php?ID=$id' class='LinkItem'><h3>" . $row['Namn'] . "</h3>";
+        echo "<img src='../Images/ProductImage$ImageID.jpg' />";
+  			echo "<br /><br /> <b>Price:</b> $" . $row['Pris'];
+  			echo " <b>In Stock:</b> " . $row['LagerAntal'] . "</a></li>";
 
 
 
-	$i++;
-	}
-	echo "</ul>";
 
-?>
-</div>
+    	  $i++;
+    	}
+    	echo "</ul>";
 
+    ?>
+    </div>
+  </div>
 
+  <canvas class="parallax"></canvas>
 </body>
 </html>
--- Php/ViewProduct.php
+++ Php/ViewProduct.php
@@ -9,84 +9,83 @@
 
 </head>
 
-
 <body>
 
-<?php
-  if(isset($_SESSION['username'])){
-    include "../Html/LogIN.html";
-  }
-  else{
-    include "../Html/menu.html";
-  }
-
-?>
-    <div class="parallax"></div>
-    <div id="wrap">
-<?php
-
-  try{
-		$db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=adasaw5db', 'adasaw-5', '1234');
-	}
-	catch(Exception $e){
-
-		echo $e->getMessage();
-	}
-
-	$id = $_GET['ID'];
-
-  $stmt = $db->prepare("SELECT * FROM Produkter WHERE ID=$id");
-	$stmt->execute();
-
-	$row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-	$lagerAntal = $row['LagerAntal'];
-  $ImageID = $row['Bild'];
-
-	echo "<h3>" . $row['Namn'] . "</h3>";
-  echo "<img src='../Images/ProductImage$ImageID.jpg' />";
-	echo "<br /> <b>Description</b> <div class='description'>" . $row['Beskrivning'] . "</div>";
-	echo "<br /> <b>Price:</b> $" . $row['Pris'];
-	echo " <b>In Stock:</b> " . $row['LagerAntal'];
-
-
-	if($_SERVER['REQUEST_METHOD']=='POST'){
-		$random = rand();
-		$stmt = $db->prepare("INSERT INTO Ordrar (ID,Datum,Status,Username,Ok) VALUES ('" . ($random) . "', '" . date("Y-m-d") . "', '" . "ok" . "', '" . ($_SESSION['username']) . "','" . 0 . "') ON DUPLICATE KEY UPDATE ID=". rand());
-		$stmt->execute();
-		$stmt1 = $db->prepare("INSERT INTO Produkter_Ordrar(Produkter_ID, Ordrar_ID, Antal) VALUES ('".$id."','". $random ."', '". $_POST['antal']. "' )");
-		$stmt1->execute();
-		$rowOP = $stmt1->fetch(PDO::FETCH_ASSOC);
-		$rowO = $stmt->fetch(PDO::FETCH_ASSOC);
-
-		$_SESSION['ProductID'] = $id;
-
-		echo "<br> Added to your cart <br>";
-	}
+  <?php
+    if(isset($_SESSION['username'])){
+      include "../Html/LogIN.html";
+    }
+    else{
+      include "../Html/menu.html";
+    }
 
   ?>
-  <div id="largeOrder"> </div>
+  <div class="parallax"></div>
+  <div id="wrap">
+    <?php
+
+      try{
+    		$db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=adasaw5db', 'adasaw-5', '1234');
+    	}
+    	catch(Exception $e){
+
+    		echo $e->getMessage();
+    	}
+
+    	$id = $_GET['ID'];
+
+      $stmt = $db->prepare("SELECT * FROM Produkter WHERE ID=$id");
+    	$stmt->execute();
+
+    	$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    	$lagerAntal = $row['LagerAntal'];
+      $ImageID = $row['Bild'];
+
+    	echo "<h1>" . $row['Namn'] . "</h1>";
+      echo "<img class='productImage' src='../Images/ProductImage$ImageID.jpg' />";
+    	echo "<div class='description'><h3>Description</h3>" . $row['Beskrivning'] . "</div>";
+    	echo "<br /> <b>Price:</b> $" . $row['Pris'];
+    	echo " <b>In Stock:</b> " . $row['LagerAntal'];
+
+
+    	if($_SERVER['REQUEST_METHOD']=='POST'){
+    		$random = rand();
+    		$stmt = $db->prepare("INSERT INTO Ordrar (ID,Datum,Status,Username,Ok) VALUES ('" . ($random) . "', '" . date("Y-m-d") . "', '" . "ok" . "', '" . ($_SESSION['username']) . "','" . 0 . "') ON DUPLICATE KEY UPDATE ID=". rand());
+    		$stmt->execute();
+    		$stmt1 = $db->prepare("INSERT INTO Produkter_Ordrar(Produkter_ID, Ordrar_ID, Antal) VALUES ('".$id."','". $random ."', '". $_POST['antal']. "' )");
+    		$stmt1->execute();
+    		$rowOP = $stmt1->fetch(PDO::FETCH_ASSOC);
+    		$rowO = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    		$_SESSION['ProductID'] = $id;
+
+    		echo "<br> Added to your cart <br>";
+    	}
+
+      ?>
+      <div id="largeOrder"> </div>
 
 
 
-	<form action="" method="post">
-      <label for="antal">Amount:</label>
-  		<input type="text" onkeyup="checkAmount()" id="antal" name="antal" value="<?php if(isset($_POST['antal'])) echo $_POST['antal'];?>">
-  		<input type="submit" id="btnSubmit" name="btnSubmit" value="Add to cart" disabled>
-	</form>
+    	<form action="" method="post">
+        <label for="antal">Amount:</label>
+      	<input type="text" onkeyup="checkAmount()" id="antal" name="antal" value="<?php if(isset($_POST['antal'])) echo $_POST['antal'];?>">
+      	<input type="submit" id="btnSubmit" name="btnSubmit" value="Add to cart" disabled>
+    	</form>
 
 
-<?php
-//checks if username is set and creates the javascript variable lagerAntal
-  echo '<script type="text/javascript">';
-  echo 'var lagerAntal = ' . json_encode($lagerAntal) . ';';
-  if(!isset($_SESSION['username'])){
-    echo "document.getElementById('btnSubmit').disabled = true;";
-    echo "document.getElementById('antal').disabled = true;";
-    echo "document.getElementById('largeOrder').innerHTML = 'Log in to buy';";
-  }
-  echo '</script>';
-?>
+    <?php
+    //checks if username is set and creates the javascript variable lagerAntal
+      echo '<script type="text/javascript">';
+      echo 'var lagerAntal = ' . json_encode($lagerAntal) . ';';
+      if(!isset($_SESSION['username'])){
+        echo "document.getElementById('btnSubmit').disabled = true;";
+        echo "document.getElementById('antal').disabled = true;";
+        echo "document.getElementById('largeOrder').innerHTML = 'Log in to buy';";
+      }
+      echo '</script>';
+    ?>
   </div>
 </body>
 </html>
