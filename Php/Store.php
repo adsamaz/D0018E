d7911fe48abcd12c96cf066b0242e1d0ea016fcc
adsamaz <adsamaz@github.com>
2017-11-29 17:13:28 +0100

    Snyggade till store + gradiant

--- Php/Store.php
+++ Php/Store.php
@@ -21,56 +21,51 @@
   ?>
   <canvas class="parallax"></canvas>
   <div id="wrap">
-    <div class="content">
-      <h1>Vapes</h1>
-    	<form class="searchForm" action ="Store.php" method="post">
-    			<input type="text" class="searchField" name="search" placeholder="Search" />
-    			<input type="submit" class="searchButton" name="btnSearch" value="Search" />
-    	</form>
+    <h1>Vapes</h1>
+  	<form class="searchForm" action ="Store.php" method="post">
+  			<input type="text" class="searchField" name="search" placeholder="Search" />
+  			<input type="submit" class="searchButton" name="btnSearch" value="Search" />
+  	</form>
 
-      <?php
-      try{
-    		$db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=adasaw5db', 'adasaw-5', '1234');
-    	}
-    	catch(Exception $e){
+    <?php
+    try{
+  		$db = new PDO('mysql:host=127.0.0.1;port=3306;dbname=adasaw5db', 'adasaw-5', '1234');
+  	}
+  	catch(Exception $e){
 
-    		echo $e->getMessage();
-    	}
-    	if(isset($_POST['search'])){
-    		$searchValue = $_POST['search'];
-    		$stmt = $db->prepare("SELECT * FROM Produkter WHERE kategori LIKE '%$searchValue%' OR Namn LIKE '%$searchValue%'");
-    	}
-    	else if(isset($_GET['category'])){
-        $category = $_GET['category'];
-        $stmt = $db->prepare("SELECT * FROM Produkter WHERE kategori = '$category'");
-      }
-      else{
-        $stmt = $db->prepare("SELECT * FROM Produkter");
-      }
-      $stmt->execute();
+  		echo $e->getMessage();
+  	}
+  	if(isset($_POST['search'])){
+  		$searchValue = $_POST['search'];
+  		$stmt = $db->prepare("SELECT * FROM Produkter WHERE kategori LIKE '%$searchValue%' OR Namn LIKE '%$searchValue%'");
+  	}
+  	else if(isset($_GET['category'])){
+      $category = $_GET['category'];
+      $stmt = $db->prepare("SELECT * FROM Produkter WHERE kategori = '$category'");
+    }
+    else{
+      $stmt = $db->prepare("SELECT * FROM Produkter");
+    }
+    $stmt->execute();
 
-    	$i = 0;
-    	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    		//if($i >= 5){
-    			//$i = 1;
-    			//echo "<div class='clear'> </div>"; //4 products in a row
-    		//}
-        $id = $row['ID'];
-        $ImageID = $row['Bild'];
+  	$i = 0;
+  	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+  		//if($i >= 5){
+  			//$i = 1;
+  			//echo "<div class='clear'> </div>"; //4 products in a row
+  		//}
+      $id = $row['ID'];
+      $ImageID = $row['Bild'];
 
-        echo "<a href='ViewProduct.php?ID=$id'><div class='productBox'><h3>" . $row['Namn'] . "</h3>";
-        echo "<img src='../Images/ProductImage$ImageID.jpg' />";
-        echo "<br /><br /> <b>Price:</b> $" . $row['Pris'];
-        echo " <b>In Stock:</b> " . $row['LagerAntal'] . "</div></a>";
+      echo "<a href='ViewProduct.php?ID=$id'><div class='productBox'><h3>" . $row['Namn'] . "</h3>";
+      echo "<img src='../Images/ProductImage$ImageID.jpg' />";
+      echo "<br /><br /> <b>Price:</b> $" . $row['Pris'];
+      echo " <b>In Stock:</b> " . $row['LagerAntal'] . "</div></a>";
 
-
-
-    	  $i++;
-    	}
-    	//echo "</ul>";
+  	  $i++;
+  	}
 
     ?>
-    </div>
   </div>
 
   <canvas class="parallax"></canvas>
